cc2d92702626c981cd39a0c34f0d1da1816106a8
Jason Waid <47893817+MasterNosaj64@users.noreply.github.com>
2024-02-15 14:01:32 -0500

    Add files via upload

---
+++ Animation.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Frame.h"
+class Animation
+{
+	char* animationName;
+	Frame* frames;
+public:
+	Animation();
+	~Animation();
+	void InsertFrame();
+	void EditFrame();
+	void DeleteFrame();
+	void ReportAnimation();
+};
---
+++ Frame.h
@@ -0,0 +1,10 @@
+#pragma once
+class Frame {
+	char* frameName;
+	Frame* pNext;
+public:
+	Frame();
+	~Frame();
+	char*& GetFrameName() { return frameName; }
+	Frame*& GetpNext() { return pNext; }
+};
